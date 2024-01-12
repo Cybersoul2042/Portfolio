e549a6cefc427394ce1f93e3cd32aeeda75c6d09
Cybersoul2042 <amirsekiroguru@gmail.com>
2024-01-12 18:38:26 +0330

    More Style

--- pfwapp/static/pfwapp/index.js
+++ pfwapp/static/pfwapp/index.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     var _a, _b, _c, _d;
-    (_a = document.querySelector('#home-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => ChangePage("home"));
-    (_b = document.querySelector('#project-btn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => ChangePage("projects"));
-    (_c = document.querySelector('#about-btn')) === null || _c === void 0 ? void 0 : _c.addEventListener('click', () => ChangePage("about"));
-    (_d = document.querySelector('#contact-btn')) === null || _d === void 0 ? void 0 : _d.addEventListener('click', () => ChangePage("contact"));
-    ChangePage("home");
+    (_a = document.querySelector('#home-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => ChangePage("home", "home-btn"));
+    (_b = document.querySelector('#project-btn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => ChangePage("projects", "project-btn"));
+    (_c = document.querySelector('#about-btn')) === null || _c === void 0 ? void 0 : _c.addEventListener('click', () => ChangePage("about", "about-btn"));
+    (_d = document.querySelector('#contact-btn')) === null || _d === void 0 ? void 0 : _d.addEventListener('click', () => ChangePage("contact", "contact-btn"));
+    ChangePage("home", "home-btn");
 });
-function ChangePage(pagetype) {
+function ChangePage(pagetype, pageButton) {
+    let pageBtns = document.querySelectorAll('.button');
+    pageBtns.forEach(btn => {
+        if (btn.id === `${pageButton}`) {
+            btn.style.borderBottom = "1px solid #a594fd";
+        }
+        else {
+            btn.style.borderBottom = "none";
+        }
+    });
     const pages = document.querySelectorAll(".page");
     pages.forEach(page => {
         if (page.id === `${pagetype}`) {
--- pfwapp/static/pfwapp/index.ts
+++ pfwapp/static/pfwapp/index.ts
@@ -1,14 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(){
-    document.querySelector('#home-btn')?.addEventListener('click', () => ChangePage("home"));
-    document.querySelector('#project-btn')?.addEventListener('click', () => ChangePage("projects"));
-    document.querySelector('#about-btn')?.addEventListener('click', () => ChangePage("about"));
-    document.querySelector('#contact-btn')?.addEventListener('click', () => ChangePage("contact"));
+    document.querySelector('#home-btn')?.addEventListener('click', () => ChangePage("home", "home-btn"));
+    document.querySelector('#project-btn')?.addEventListener('click', () => ChangePage("projects", "project-btn"));
+    document.querySelector('#about-btn')?.addEventListener('click', () => ChangePage("about", "about-btn"));
+    document.querySelector('#contact-btn')?.addEventListener('click', () => ChangePage("contact", "contact-btn"));
 
-    ChangePage("home");
+    ChangePage("home", "home-btn");
 })
 
-function ChangePage(pagetype: string)
+function ChangePage(pagetype: string, pageButton: string)
 {
+    let pageBtns = document.querySelectorAll<HTMLElement>('.button');
+    
+    pageBtns.forEach(btn => {
+        if(btn.id === `${pageButton}`)
+        {
+            
+            btn!.style.borderBottom = "1px solid #a594fd";
+            
+        }
+        else
+        {
+            btn!.style.borderBottom = "none";
+        }
+    });
+    
+    
     const pages = document.querySelectorAll<HTMLElement>(".page")
     
     pages!.forEach(page => {
